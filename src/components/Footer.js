cafa02120601fd892b6bd8052f233b15218b2988
Aditya <rizky.aditya6565@gmail.com>
2024-12-24 18:16:18 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div>
+      <h2>Ini Adalah Components Footer</h2>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bulma/css/bulma.css";
+
+const root = ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
